bc794e2c04b88175e620cac44512fe728f39eb9a
DennisSmuda <dennissmuda@msn.com>
2014-02-22 14:30:46 +0100

    Bugfix.

--- js/flappy.js
+++ js/flappy.js
@@ -21,14 +21,16 @@ game_state.main.prototype = {
         var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         space_key.onDown.add(this.jump, this);
         
-        this.pipes = game.add.group;
+        this.pipes = game.add.group();
         this.pipes.createMultiple(20, 'pipe');
+        
+        this.timer = this.game.time.events.loop(1500, this.add_row_of_pipesm this);
         
         this.score = 0;
         var style = {font: "30px Arial", fill: "#ffffff" };
         this.label_score = this.game.add.text(20, 20, "0", style);
         
-        this.timer = this.game.time.events.loop(1500, this.add_row_of_pipesm this);
+        
         
     },
 
@@ -36,6 +38,7 @@ game_state.main.prototype = {
     update: function() {
 		if(this.bird.inWorld == false) 
             this.restart_game();
+        
         this.game.physics.overlap(this.bird, this.pipes, this.restart_game, null, this);
     },
     
@@ -73,8 +76,6 @@ game_state.main.prototype = {
     
     
 };
-    
-
 
 // Add and start the 'main' state to start the game
 game.state.add('main', game_state.main);  
