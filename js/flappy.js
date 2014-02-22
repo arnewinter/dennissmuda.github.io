bf04e7a30f1aa3a424bc368be7c920c47995a5d6
DennisSmuda <dennissmuda@msn.com>
2014-02-22 13:23:17 +0100

    Implemented basic framework

--- js/flappy.js
+++ js/flappy.js
@@ -7,18 +7,37 @@ game_state.main = function() { };
 game_state.main.prototype = {
 
     preload: function() { 
-		// Function called first to load all the assets
+		this.game.stage.backgroundColor = '#71c5cf';
+        this.game.load.image('bird', 'assets/bird.png');
     },
 
     create: function() { 
-    	// Fuction called after 'preload' to setup the game    
+    	this.bird = this.game.add.sprite(100, 245, 'bird');
+        this.bird.body.gravity.y = 1000;
+        
+        var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        space_key.onDown.add(this.jump, this);
     },
     
     update: function() {
-		// Function called 60 times per second
+		if(this.bird.inWorld == false) 
+        this.restart_game();
     },
 };
 
 // Add and start the 'main' state to start the game
 game.state.add('main', game_state.main);  
 game.state.start('main'); 
+
+
+
+jump: function() {
+    this.bird.body.velocity.y = -350;
+},
+    
+    // Restart the game
+restart_game:function(){
+    this.game.state.start('main');
+},
+    
+    
