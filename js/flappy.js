ca2d07eba757349387936d620d3e8dc796de1bdd
DennisSmuda <dennissmuda@msn.com>
2014-02-22 13:38:09 +0100

    blabla

--- js/flappy.js
+++ js/flappy.js
@@ -7,37 +7,18 @@ game_state.main = function() { };
 game_state.main.prototype = {
 
     preload: function() { 
-		this.game.stage.backgroundColor = '#71c5cf';
-        this.game.load.image('bird', 'assets/bird.png');
+		// Function called first to load all the assets
     },
 
     create: function() { 
-    	this.bird = this.game.add.sprite(100, 245, 'bird');
-        this.bird.body.gravity.y = 1000;
-        
-        var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        space_key.onDown.add(this.jump, this);
+    	// Fuction called after 'preload' to setup the game    
     },
     
     update: function() {
-		if(this.bird.inWorld == false) 
-        this.restart_game();
+		// Function called 60 times per second
     },
 };
 
 // Add and start the 'main' state to start the game
 game.state.add('main', game_state.main);  
 game.state.start('main'); 
-
-
-
-jump: function() {
-    this.bird.body.velocity.y = -350;
-},
-    
-    // Restart the game
-restart_game:function(){
-    this.game.state.start('main');
-},
-    
-    
